0f27f1d4d6287b069a39f464eda0ac35a3171cd3
Snehal979 <bansodsnehal535@gmail.com>
2023-03-25 01:34:58 +0530

    [Snehal]Add:create object for collection

--- AddressBook_UsingLINQ/Program.cs
+++ AddressBook_UsingLINQ/Program.cs
@@ -5,6 +5,8 @@
         public static void Main(String[] args)
         {
             Console.WriteLine("AddressBook management Using LINQ to Object with Query Syntax and Lambda Expression");
+            
+            List<PersonData> personDatas = new List<PersonData>();
             Console.ReadLine();
         }
     }
