d4ab6f60872f2f7df8b28442207565ad8331460c
Snehal979 <bansodsnehal535@gmail.com>
2023-03-25 01:26:33 +0530

    [Snehal]Add:Welcome msg For AddressBook Management

---
+++ AddressBook_UsingLINQ/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace AddressBook_UsingLINQ
+{
+    class Program
+    {
+        public static void Main(String[] args)
+        {
+            Console.WriteLine("AddressBook management Using LINQ to Object with Query Syntax and Lambda Expression");
+            Console.ReadLine();
+        }
+    }
+}
